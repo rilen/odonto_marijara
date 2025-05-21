90aea4802e9abc4bbfe7b0678f2a08a453b2cc85
Rilen T.L. <rilen.lima@gmail.com>
2025-05-21 19:51:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
+```
 import { defineConfig } from 'vite';
-   import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
-   export default defineConfig({
-     plugins: [react()],
-     build: {
-       outDir: 'dist',
-     },
-     server: {
-       proxy: {
-         '/api': 'http://localhost:3000',
-       },
-     },
-     optimizeDeps: {
-       include: ['@fullcalendar/core', '@fullcalendar/timegrid', '@fullcalendar/daygrid', '@fullcalendar/interaction', '@fullcalendar/react'],
-     },
-   });
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  optimizeDeps: {
+    include: ['@fullcalendar/core', '@fullcalendar/timegrid', '@fullcalendar/daygrid', '@fullcalendar/interaction', '@fullcalendar/react'],
+  },
+});
+```

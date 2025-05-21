171eb60d6ca7e714a7e4d6bf9bb1c27d22b5cb73
Rilen T.L. <rilen.lima@gmail.com>
2025-05-21 20:52:13 -0300

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -44,5 +44,5 @@
      });
 
      const PORT = process.env.PORT || 3000;
-     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+     app.listen(PORT, () => console.log("Servidor rodando na porta ${PORT}"));
      ```

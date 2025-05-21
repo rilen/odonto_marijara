b55a01958d93a677af3bf4ea2acc7abb197022cc
Rilen T.L. <rilen.lima@gmail.com>
2025-05-21 19:53:34 -0300

    Update User.js

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,3 +1,4 @@
+```
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -8,3 +9,4 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+```
